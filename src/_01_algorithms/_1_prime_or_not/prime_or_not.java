b8b4dbde8de42557824250e62b6c17f3d2aad230
root <root@5d0b8803d45f>
2020-06-28 21:28:38 +0000

    6/28/20... saving stuff ye

---
+++ src/_01_algorithms/_1_prime_or_not/prime_or_not.java
@@ -0,0 +1,13 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class prime_or_not {
+	public static void main(String[] args) {
+		
+		String number = JOptionPane.showInputDialog(null, "Gimme a number :D");
+		for(int i=0; i>0; i++);
+		//if(number % 2==0) {
+		
+	}
+}
