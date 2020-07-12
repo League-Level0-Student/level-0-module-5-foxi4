44c113b48217bbdfa946de8108b94602754bed7c
root <root@80ca9e529c4d>
2020-07-12 21:27:08 +0000

    7/12/20... ye

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "Potato.";
+		String goofyName = "";
 
 		// 1. Ask the user to enter their name
 String name=JOptionPane.showInputDialog("What is your name");
@@ -27,15 +27,18 @@ for(int i=0;i<name.length();i++) {
 
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-	if(i % 2==0) {
-
+	if(i % 2==1) {
+		variable=variable.toLowerCase();
 	}
-			
+	if(i % 2==0)
+		variable=variable.toUpperCase();
 			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+//um its only doing upper case
+	goofyName+=variable;
 }
 		// 7. Use pop-up to show user their Goofy name
-
+System.out.print(goofyName);
 	}
 }
-//can't hear yall cuz there is music playing :}
+
+//can't hear yall cuz there is music playing :} <- yeah
--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,54 +2,65 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
+
+//music is louddd
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
-
+Robot iCU=new Robot();
+iCU.miniaturize();
 		// #2. Set your robot’s position to x=300 and y=300
 
-
+iCU.moveTo(300, 300);
 		// #3. Put the robot's pen down
 
-
+iCU.penDown();
 		// #4. Set the robot’s speed to 5
 
-
+iCU.setSpeed(100);
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
 
+			for(int i=0;i<5;i++) {
+				
 			
 			// #6. Set the pen color to black
-
+				
+iCU.setPenColor(Color.black);
 			
 			// #7. move the robot by 50
-
+iCU.move(50);
 			
 			// #8. turn the robot to the right by 60
+			iCU.turn(60);
 			
-			
-			// #12. set the pen color to blue for even numbers of i
-
+			// #12. set the pen color to blue for even numbers of i (what does it mean even (odd) numbers of i?)
+			if(i % 2==0) {
+iCU.setPenColor(Color.blue);
+			}
 			
 			// #13. set the pen color to red for all odd numbers of i
-			
-			
+			if(i % 2==1) {
+				iCU.setPenColor(Color.red);
+			}
+
+			}
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
+			for(int j=0;j<5;j++);
 			
 					// #10. turn the robot to the left by 60
-		
+		iCU.turn(300);
 					
 					// #11. move the robot by 50 (play the start button)
-					
+					iCU.move(50);
 				
-			
-			// #14. set the pen color to black
+					// #14. set the pen color to black
+					iCU.setPenColor(Color.black);
 				
-		
+		//music is loud :D, so be loud so i can hear u :9
 	}
 
 }
