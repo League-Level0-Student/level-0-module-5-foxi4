34d645dff9d7906a93ba3e3e47d4e708a5efb512
root <root@c0e0499356bc>
2020-07-19 21:28:28 +0000

    7/19/20... ye

--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 
-//music is louddd
+//music is louddd *owo*
 public class Snowflake {
 	
 	public static void main(String[] args) {
@@ -21,15 +21,15 @@ iCU.moveTo(300, 300);
 iCU.penDown();
 		// #4. Set the robot’s speed to 5
 
-iCU.setSpeed(100);
+iCU.setSpeed(44);
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
 
-			for(int i=0;i<5;i++) {
+			for(int i=0;i<6;i++) {
 				
 			
 			// #6. Set the pen color to black
 				
-iCU.setPenColor(Color.black);
+iCU.setPenColor(Color.green);
 			
 			// #7. move the robot by 50
 iCU.move(50);
@@ -39,7 +39,7 @@ iCU.move(50);
 			
 			// #12. set the pen color to blue for even numbers of i (what does it mean even (odd) numbers of i?)
 			if(i % 2==0) {
-iCU.setPenColor(Color.blue);
+iCU.setPenColor(Color.white);
 			}
 			
 			// #13. set the pen color to red for all odd numbers of i
@@ -47,9 +47,9 @@ iCU.setPenColor(Color.blue);
 				iCU.setPenColor(Color.red);
 			}
 
-			}
+			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			for(int j=0;j<5;j++);
+			for(int j=0;j<6;j++) {
 			
 					// #10. turn the robot to the left by 60
 		iCU.turn(300);
@@ -58,8 +58,9 @@ iCU.setPenColor(Color.blue);
 					iCU.move(50);
 				
 					// #14. set the pen color to black
-					iCU.setPenColor(Color.black);
-				
+					iCU.setPenColor(255,255,0);
+			}
+			}
 		//music is loud :D, so be loud so i can hear u :9
 	}
 
--- src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
+++ src/_02_nested_loops/_2_amazing_rings/amazing_rings/amazing_rings.pde
@@ -1,10 +1,37 @@
-
-void setup() {
-  
+void setup(){
+  size(1000,450);
+}
+void draw(){
+  int size=400;
+  for(int i=0;i<80;i++){
+    int ellipse=i;
+    if(ellipse % 2==0){
+  noFill();
+    }
+    
+    else{
+      noFill();
+    }
+  ellipse(250,225,size,size);
+  size-=10;
+    
+  }
+   size=400;
+  for(int i=0;i<80;i++){
+    int ellipse=i;
+    if(ellipse % 2==0){
+  noFill();
+    }
+    
+    else{
+      noFill();
+    }
+  ellipse(750,225,size,size);
+  size-=10;
+    
+  }
 }
 
-void draw() {
-  
   // Go to the recipe to run the demonstration before starting this program
   
   /*
@@ -13,4 +40,3 @@ void draw() {
   Make the rings move past each other.
   When the rings hit the side of the sketch, make them reverse their direction.
   */
-}
