7eb9ff18aa34e0bc13056bfbecb52de2a0b758c8
laurenkan. <laurenkan.@localhost>
2020-08-16 14:31:54 -0700

    8/16/30

--- src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoopsIDK.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoopsIDK.java
@@ -2,112 +2,93 @@ package _02_nested_loops._3_for_loop_gauntlet;
 
 public class NestedLoopsIDK {
 	public static void main(String[] args) {
-//music ON and loud lol
-		//for(int i=0;i<101;i++) {
-			//System.out.println(i);
-		//}
-		//Instructions
-		
-		//1. done
-		
-	//for(int i=100; i>0;i--) {
-		//System.out.println(i);
-		//}
-		//2. done, if I comment out rest
-	//for(int i=2; i<102;i++) {
-		//if(i%2==0) {
-		//System.out.print(i);
-		//}
-		//	}
-		//3.done, if comment out rest
-		//for(int i=1; i<100;i++) {
-			//if(i%2==1) {
-				//System.out.println(i);
-			//}
-		//}
-		//4. done, if comment out rest (code is up)
-		//for(int i=0; i<501; i++) {
+		// music ON and loud lol
+		// for(int i=0;i<101;i++) {
+		// System.out.println(i);
+		// }
+		// Instructions
 
-		//	if(i%2==0) {
+		// 1. done
 
-			//	System.out.println(i + " is even. ");
-			//}
-			//if(i%2==1) {
+		// for(int i=100; i>0;i--) {
+		// System.out.println(i);
+		// }
+		// 2. done, if I comment out rest
+		// for(int i=2; i<102;i++) {
+		// if(i%2==0) {
+		// System.out.print(i);
+		// }
+		// }
+		// 3.done, if comment out rest
+		// for(int i=1; i<100;i++) {
+		// if(i%2==1) {
+		// System.out.println(i);
+		// }
+		// }
+		// 4. done, if comment out rest (code is up)
+		// for(int i=0; i<501; i++) {
 
-			//	System.out.println(i + " is odd. ");
-			//}
-		//}
-			//how do you make spaces where each sentence makes a new sentence?=~=
-			// change print to println
-			//woooooooooooooahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-		
-		//5. done ;]
-	//for(int i=0; i<784;i++) {
-	//	if(i%7==0) {
-	//	System.out.println(i);
-	//	}
-//	}
-		//6. done... next class do 7 hehe
+		// if(i%2==0) {
 
-			
-		//for(int i=2008; i<2020; i++) {
-			//System.out.println("in " +i+ " i was " +(i-2008)); 
-			
-			
-		//}
-		//System.out.println("in 2020 i am 12");
-			//I don't know how to connect them
-	
-		//7.done
-	
-		//for(int i=0; i<3; i++) {
-		//	for(int j=0; j<3; j++) {
-			
-		//	System.out.println(i+" " +j);
-			
-		//}}
-		
-		//Nested For-Loops - 1. done
-		
-		for(int i=0; i<10; i++) {
-			for(int j=0; j<2; j++) {
-				String cat = "the value of the string"; 
-			System.out.println(i+ " "+j);
-					
+		// System.out.println(i + " is even. ");
+		// }
+		// if(i%2==1) {
+
+		// System.out.println(i + " is odd. ");
+		// }
+		// }
+		// how do you make spaces where each sentence makes a new sentence?=~=
+		// change print to println
+		// woooooooooooooahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+
+		// 5. done ;]
+		// for(int i=0; i<784;i++) {
+		// if(i%7==0) {
+		// System.out.println(i);
+		// }
+		// }
+		// 6. done... next class do 7 hehe
+
+		// for(int i=2008; i<2020; i++) {
+		// System.out.println("in " +i+ " i was " +(i-2008));
+
+		// }
+		// System.out.println("in 2020 i am 12");
+		// I don't know how to connect them
+
+		// 7.done
+
+		// for(int i=0; i<3; i++) {
+		// for(int j=0; j<3; j++) {
+
+		// System.out.println(i+" " +j);
+
+		// }}
+
+		// Nested For-Loops - 1. done
+
+		// int counter = 1;
+		// for (int i = 0; i < 3; i++) {
+		// for (int j = 0; j < 3; j++) {
+
+		// System.out.print(counter);
+		// counter++;
+
+		// }
+		// System.out.println();
+
+		// }
+		// Nested For-Loops - 2. done
+		int number=1;
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.print(number);
+				number++;
+			}
+			System.out.println();
 		}
-	//Nested For-Loops - 2. 
-}//um something messed up is happening
-}\}
+	
+		// Nested For-loops - 3. done
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
+}
