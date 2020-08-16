8ac82535e3f109d9097d035a30fd5a4d692dcac5
laurenkan. <laurenkan.@localhost>
2020-08-16 13:50:12 -0700

    have to change to other eclipse cuz this one is messed up 8/16/20

--- src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoopsIDK.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/NestedLoopsIDK.java
@@ -59,13 +59,23 @@ public class NestedLoopsIDK {
 			//I don't know how to connect them
 	
 		//7.done
-		//MUSIC PLAYING :}
-		for(int i=0; i<2; i++) {
-			System.out.println(i);
+	
+		//for(int i=0; i<3; i++) {
+		//	for(int j=0; j<3; j++) {
+			
+		//	System.out.println(i+" " +j);
+			
+		//}}
+		
+		//Nested For-Loops - 1. done
+		
+		//for(int i=0; i<10; i++) {
+			//for(int j=0; j<2; j++) {
+				String cat = "the value of the string"; 
+					
 		}
-		//continue and ask for help~ ;)
-		//Nested For-Loops - 1.
-}
+	//Nested For-Loops - 2. 
+}//um something messed up is happening
 }
 
 
