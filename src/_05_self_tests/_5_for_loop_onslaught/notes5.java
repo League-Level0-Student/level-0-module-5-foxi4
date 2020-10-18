105979df1f16d591d13c0b2acd75cc1a6fc677a3
laurenkan. <laurenkan.@localhost>
2020-10-18 14:28:12 -0700

    yaeriufthjvoic 10/18/20

---
+++ src/_05_self_tests/_5_for_loop_onslaught/notes5.java
@@ -0,0 +1,12 @@
+package _05_self_tests._5_for_loop_onslaught;
+
+public class notes5 {
+//wait a sec... do you really need a main if you aren't commanding anything? yah i think im good.
+	//NOTES um
+	//pay extremely close attention to every detail. even spaces are wrong sometimes
+	//YOU MUST BE EXACT BEFORE TURNING IN
+	//TAKE YOUR TIME- um unless you are on a tight time limit then hurry a lil but always..
+	// DOUBLE CHECK YE
+	//(☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜) uwu
+	
+}
