7be5b6c0050deff549393b3580e87a6b2b27c91a
laurenkan. <laurenkan.@localhost>
2020-11-01 14:43:30 -0800

    poo poo lol (11/1/20)

--- src/_05_self_tests/_1_which_data_type_1/notes.java
+++ src/_05_self_tests/_1_which_data_type_1/notes.java
@@ -9,6 +9,7 @@ public static void main(String[] args) {
 	//boolean is for...
 	//String is for words, like: String location = "San Diego";
 	//  p o o  :)
+	//writing this because I need some changes
 	
 	
 	
